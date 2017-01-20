0b2297a3c8403e9494f01e1a7427c3902e663427
Andre Teodoro <alteodoro@yahoo.com>
2017-01-20 18:28:16 -0200

    Adding geocoding solution

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'carrierwave-base64'
 gem 'mini_magick'
 gem 'cloudinary'
 
+# Ruby geocoding solution
+gem 'geocoder'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,15 @@
 class Address < ApplicationRecord
+  # Get the geocode after validating the data
+  after_validation :geocode
+
+  geocoded_by :full_address do |obj, results|
+    if geo = results.first
+      obj.latitude = geo.latitude
+      obj.longitude = geo.longitude
+    end
+  end
+
+  def full_address
+    "#{street}, #{neighborhood}, #{city} #{number}, #{country}"
+  end
 end
