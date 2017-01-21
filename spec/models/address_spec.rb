0a55ec8945905ab88b7f8b9dcabf755e5c2db93a
Andre Teodoro <alteodoro@yahoo.com>
2017-01-21 16:28:44 -0200

    Tests for geocode

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}" 
+  describe 'Geolocation - latitude and longitude' do
+    context 'with valid address' do
+      it 'returns the right latitude and longitude' do
+        @address = create(:address, country: 'Brazil', state: 'Sao Paulo', city: 'Sao Paulo', neighborhood: 'Bela Vista', street: 'Av Paulista', number: '1000')
+        expect(@address.latitude.to_f).to eql(-23.5650071)
+        expect(@address.longitude.to_f).to eql(-46.6520933)
+      end
+    end
+
+    context 'with blank address' do
+      it 'returns cordinates for an unknown place' do
+        @address = create(:address, country: 'na', state: 'na', city: 'na', neighborhood: 'na', street: 'na', number: 'na')
+        expect(@address.latitude.to_f).to eql(-22.95764)
+        expect(@address.longitude.to_f).to eql(18.49041)
+
+
+      end
+    end
+  end
 end
