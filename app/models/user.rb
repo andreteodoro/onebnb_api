b31b6fdbf866cf91b781de8b0d89aff5101d3152
Andre Teodoro <alteodoro@yahoo.com>
2017-01-24 16:52:21 -0200

    Adding seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   mount_base64_uploader :photo, PhotoUploader
   has_many :wishlists
+  has_many :properties
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
