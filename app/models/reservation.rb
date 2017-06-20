b826a376dbabb96083049c7c6f26f5ead355003e
Andre Teodoro <alteodoro@yahoo.com>
2017-06-20 20:57:33 -0300

    Revert "Creating payments infrastructure"
    This reverts commit 52ed48a975a4a311e9549b8a643f7f90653f5968.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,34 +9,6 @@ class Reservation < ApplicationRecord
   before_create :set_pending_status
   validates_presence_of :property, :user
 
-
-  def self.create_order reservation_params, card_params
-    Reservation.transaction do
-      reservation = self.create(reservation_params)
-      Transaction.create(reservation: reservation, user: reservation.user, price: reservation.price, status: :pending, card: Card.create(card_params))
-    end
-  end
-
-  def accept
-    Reservation.transaction do
-      self.update(status: :active)
-      res = ::MoipApi.client.post("/v2/payments/#{self.order.moip_id}/capture", {})
-      self.order.status = :success
-    end
-  end
-
-  def cancel status
-    Reservation.transaction do
-      self.update(status: status)
-      res = ::MoipApi.client.post("/v2/payments/#{self.order.moip_id}/void", {})
-      self.order.status = :failed
-    end
-  end
-
-  def price
-    ((self.interval_of_days * self.property.price) * 1.1).to_f
-  end
-
   def staying_days
     (self.checkout_date - self.checkin_date).to_i
   end
